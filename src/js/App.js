8ea8f0a531ae1b675687d776a6b254d4b432670f
Charlotta <cocos.lo@live.se>
2020-12-07 08:39:34 +0100

    Moved App

--- src/js/App.js
+++ src/js/App.js
@@ -1,15 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
+import firebase from './js/firebase.js';
+import {Sidebar} from './reactjs/sidebar.js';
+import Show from './reactjs/show.js';
+import Home from './reactjs/home.js';
+import Course from './reactjs/course.js';
+import CourseInfo from './reactjs/courseInfo.js';
+import SidebarDeadlines from './reactjs/sidebarDeadlines.js';
+import {CustomError} from './reactjs/error.js';
 
-function App() {
-  return (
-    <div>
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
-    </div>
-  );
+
+const App = ({model}) =>
+
+<div>
+
+  <div><Sidebar model={model} moveHome={homeNav} moveCourses={coursesNav} moveBooks={bookNav}/></div>
+
+  <Show hash="#home" ><div><Home model={model}/></div></Show>
+  <Show hash="#courses" ><div><Course model={model} viewNav={viewNav}/></div></Show>
+  <Show hash="#course" ><div><CourseInfo model={model} coursesNav={coursesNav} errorNav={errNav}/></div></Show>
+  <Show hash="#error" ><div><CustomError home={homeNav}/></div></Show>
+  <div><SidebarDeadlines model={model} err={errNav}/></div>
+
+</div>
+
+
+window.addEventListener("hashchange",()=> {defaultRoute()})
+
+const viewNav=()=> window.location.hash="#course";
+const bookNav=()=> window.location.hash="#home";
+const coursesNav=()=> window.location.hash="#courses";
+const errNav=()=> window.location.hash="#error";
+const homeNav=()=> window.location.hash="#home";
+
+defaultRoute();
+
+export function defaultRoute(){
+    if(!["#error","#course","#home","#createcomment","#createcommentoverview","#createdeadline","#createcourse", "#courses"].find(knownRoute=>
+    knownRoute==window.location.hash))
+	window.location.hash="#home";
 }
 
 export default App;
--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,13 +1,34 @@
-import firebase from "firebase";
+import firebaseui from 'firebaseui'
 
-firebase.initializeApp({
+const config = ({
   apiKey: "AIzaSyBQgAGR2jgG52DT_iMMKAYV87UowWabkHQ",
   authDomain: "studymouse-c539a.firebaseapp.com",
   databaseURL: "https://studymouse-c539a.firebaseio.com",
   projectId: "studymouse-c539a",
   storageBucket: "studymouse-c539a.appspot.com",
   messagingSenderId: "503177314168",
-  appId: "1:503177314168:web:1ad9e4e406fd19f474128d"
+  appId: "1:503177314168:web:1ad9e4e406fd19f474128d"})
+
+// This is our firebaseui configuration object
+const uiConfig = ({
+  signInSuccessUrl: '#home',
+  signInOptions: [
+    window.firebase.auth.EmailAuthProvider.PROVIDER_ID
+    window.firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: '/terms-of-service' // This doesn't exist yet
 })
 
-export default firebase
+// This must run before any other firebase functions
+window.firebase.initializeApp(config)
+
+// This sets up firebaseui
+const ui = new firebaseui.auth.AuthUI(window.firebase.auth())
+
+// This adds firebaseui to the page
+// It does everything else on its own
+const startFirebaseUI = function (elementId) {
+  ui.start(elementId, uiConfig)
+}
+
+export startFirebaseUI
